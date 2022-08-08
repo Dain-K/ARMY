6f817c826d51c5dbd1eb64939e4b5122d49bcc27
Dain-K <dksudi76@naver.com>
2022-08-08 20:44:53 +0900

    spring

---
+++ armyproject/src/main/java/com/example/armyproject/ArmyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.armyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmyprojectApplication.class, args);
+	}
+
+}
---
+++ armyproject/src/test/java/com/example/armyproject/ArmyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.armyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
