2a2be752428906b89e8d68e2a4a729ff513d3c1a
Dain-K <dksudi76@naver.com>
2022-08-21 01:20:52 +0900

    라디오 버튼 추가

--- ARMP_APP/app/src/main/java/com/example/myapplication/ReportViewActivity.java
+++ ARMP_APP/app/src/main/java/com/example/myapplication/ReportViewActivity.java
@@ -1,15 +1,54 @@
 package com.example.myapplication;
 
-import android.os.Bundle;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ReportViewActivity extends AppCompatActivity {
-
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_view);
+
+        radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rg_btn1:
+
+                        break;
+                    case R.id.rg_btn2:
+
+                        break;
+                }
+            }
+        });
     }
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.rg_btn1:
+                if (checked)
+
+                    break;
+            case R.id.rg_btn2:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
+    }
+
+
 }
